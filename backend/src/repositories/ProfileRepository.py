c8d08f55abe80d2fdc99317edfac4dac89ca4ef2
O. Eugenio <ome18@fsu.edu>
2023-04-19 02:04:03 -0400

    Refactored to use camelCasing.

--- backend/src/repositories/ProfileRepository.py
+++ backend/src/repositories/ProfileRepository.py
@@ -1,14 +1,22 @@
+from abc import ABC
+
 from PyDataOpsKit import AbstractRepository
 from PyDataOpsKit.DatabaseManager import DatabaseManager
+from backend.src.models.Profile import Profile
 
 
 class ProfileRepository(AbstractRepository):
 
     def __init__(self):
         self.db = DatabaseManager()
-        self.create_table()
+        self.createTable()
 
-    def create_table(self):
+    def createTable(self):
+        """
+        Creates the profiles table in the database if it does not exist.
+        :return:
+        :rtype:
+        """
         try:
             self.db.query("""
                 CREATE TABLE IF NOT EXISTS profiles (
@@ -32,16 +40,57 @@ class ProfileRepository(AbstractRepository):
         print("Profile table created successfully")
 
     def add(self, profile):
+        """
+        Adds a profile to the database
+        :param profile:
+        :type profile:
+        :return:
+        :rtype:
+        """
         print(f"Adding profile for user {profile.userID} to the database")
-        result = self.db.query("""
+        self.db.query("""
         INSERT INTO profiles (id, visibility, firstName, lastName, bio, gender, age, location, sexuality, interests, favoriteMovies)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (profile.userID, profile.visibility, profile.firstName, profile.lastName, profile.bio, profile.gender,
               profile.age, profile.location, profile.sexuality, profile.interests, profile.favoriteMovies))
-        print(result)
 
     def update(self, profile):
-        pass
+        """
+        Updates a profile in the database, returns the updated Profile object
+        :param profile:
+        :type profile:
+        :return:
+        :rtype:
+        """
+        print(f"Updating profile for user {profile.userID} in the database")
+
+        updateSQL = """
+           UPDATE profiles
+           SET visibility = %s, 
+               firstName = %s,
+               lastName = %s,
+               bio = %s,
+               gender = %s,
+               age = %s,
+               location = %s,
+               sexuality = %s,
+               interests = %s,
+               favoriteMovies = %s
+           WHERE id = %s
+           """
+
+        self.db.query(updateSQL,
+                      (profile.visibility,
+                       profile.firstName,
+                       profile.lastName,
+                       profile.bio,
+                       profile.gender,
+                       profile.age,
+                       profile.location,
+                       profile.sexuality,
+                       profile.interests,
+                       profile.favoriteMovies,
+                       profile.userID))
 
     def delete(self, profile):
         pass
@@ -49,23 +98,61 @@ class ProfileRepository(AbstractRepository):
     def get(self, profile):
         pass
 
-    def get_all(self):
+    def getAll(self):
         pass
 
-    def get_by_userID(self, userID):
-        print(userID)
-        profile = self.db.query("""
-            SELECT * FROM profiles WHERE id = %s LIMIT 1
-        """, (userID,))
+    def getByUserID(self, userID):
+        """
+        searches the database for a profile with the given userID and returns a Profile object if found.
+        :param userID:
+        :type userID:
+        :return:
+        :rtype:
+        """
+        query = self.db.query("""
+            SELECT * FROM profiles WHERE id = %s LIMIT 1;
+        """, (userID,))[0]
 
-        return profile
+        print(query)
 
-    def get_random(self, numProfilesToFetch):
-        output = self.db.query("""
+        if query:
+            return Profile(userID=query[0],
+                           visibility=query[1],
+                           firstName=query[2],
+                           lastName=query[3],
+                           bio=query[4])
+        else:
+            return None
+
+    def getRandom(self, numProfilesToFetch):
+        """
+        Fetches n-number of random number of profiles from the database.
+        :return:
+        :rtype:
+        """
+
+        query = self.db.query("""
             SELECT * FROM profiles ORDER BY RANDOM() LIMIT 5
             """)
-        print(output)
-        return output
+
+        if query is None:
+            return None
+
+        profiles = []
+        for profilesTuples in query:
+            profiles.append(Profile(userID=profilesTuples[0],
+                                    visibility=profilesTuples[1],
+                                    firstName=profilesTuples[2],
+                                    lastName=profilesTuples[3],
+                                    bio=profilesTuples[4],
+                                    gender=profilesTuples[5],
+                                    age=profilesTuples[6],
+                                    location=profilesTuples[7],
+                                    sexuality=profilesTuples[8],
+                                    interests=profilesTuples[9],
+                                    favoriteMovies=profilesTuples[10]))
+
+        return profiles
 
     def __del__(self):
         self.db.close()
