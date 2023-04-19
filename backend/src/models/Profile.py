ae8eea81b5fcdc5cdb62576217e17df29d186608
O. Eugenio <ome18@fsu.edu>
2023-04-19 02:03:43 -0400

    Refactored to use camelCasing and also implement the new required methods (toDict and toJSON) from PyDataOpsKit v1.0.1

--- backend/src/models/Profile.py
+++ backend/src/models/Profile.py
@@ -1,22 +1,49 @@
+import json
 from dataclasses import dataclass
-from PyDataOpsKit import AbstractEntity
+from PyDataOpsKit import AbstractModel
 
 
 @dataclass
-class Profile(AbstractEntity):
+class Profile(AbstractModel):
 
-    def __init__(self, userID):
+    def __init__(self,
+                 userID,
+                 visibility=False,
+                 firstName=None,
+                 lastName=None,
+                 bio=None,
+                 gender=None,
+                 age=None,
+                 location=None,
+                 sexuality=None,
+                 interests=None,
+                 favoriteMovies=None):
         self.userID = userID
-        self.visibility = False
-        self.firstName = None
-        self.lastName = None
-        self.bio = None
-        self.gender = None
-        self.age = None
-        self.location = None
-        self.sexuality = None
-        self.interests = None
-        self.favoriteMovies = None
+        self.visibility = visibility
+        self.firstName = firstName
+        self.lastName = lastName
+        self.bio = bio
+        self.gender = gender
+        self.age = age
+        self.location = location
+        self.sexuality = sexuality
+        self.interests = interests
+        self.favoriteMovies = favoriteMovies
 
-    def __str__(self):
-        return f"{self.userID} - {self.visibility}"
+    def toDict(self) -> dict:
+        return {
+            "userID": self.userID,
+            "visibility": self.visibility,
+            "firstName": self.firstName,
+            "lastName": self.lastName,
+            "bio": self.bio,
+            "gender": self.gender,
+            "age": self.age,
+            "location": self.location,
+            "sexuality": self.sexuality,
+            "interests": self.interests,
+            "favoriteMovies": self.favoriteMovies
+        }
+
+    def toJSON(self):
+        return json.dumps(self.toDict())
