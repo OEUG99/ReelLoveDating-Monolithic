49fc9f2d5cffbb659ce972569b46746c05fe08a7
O. Eugenio <ome18@fsu.edu>
2023-04-19 19:48:24 -0400

    renamed Movies to Movie to keep in line with current use of non-plurals among the code base.

---
+++ backend/src/models/Movie.py
@@ -0,0 +1,47 @@
+from PyDataOpsKit import AbstractModel
+
+
+class Movie(AbstractModel):
+    """
+    This movie class models the movie entity in the database.
+    Whenever we get a movie info from the database, we will create a movie object.
+    Whenever we want to create a new movie, we will create a movie object, and then add it to the database
+    via the movieRepository.
+    """
+
+    def __init__(self, movieID: str, name: str = None, published_date: str = None, director: str = None,
+                 genre: str = None, description: str = None, rating: int = None):
+        """
+        creates a new movie object
+        :param movieID:
+        :type movieID:
+        :param name:
+        :type name:
+        :param published_date:
+        :type published_date:
+        :param director:
+        :type director:
+        :param genre:
+        :type genre:
+        """
+        self.movieID = movieID
+        self.name = name
+        self.published_date = published_date
+        self.director = director
+        self.genre = genre
+        self.description = description
+        self.rating = rating
+
+    def toDict(self) -> dict:
+        """
+        Converts the user object to a dictionary.
+        :return:
+        :rtype:
+        """
+        return {
+            "movieID": self.movieID,
+            "name": self.name,
+            "published_date": self.published_date,
+            "director": self.director,
+            "genre": self.genre
+        }
