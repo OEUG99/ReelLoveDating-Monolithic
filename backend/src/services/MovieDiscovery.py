70d60dfd6e844dba9107c66328f97419aeaf64bd
O. Eugenio <ome18@fsu.edu>
2023-04-20 03:51:33 -0400

    Added boilerplate code for the MovieDiscovery service.

---
+++ backend/src/services/MovieDiscovery.py
@@ -0,0 +1,8 @@
+class MovieDiscovery:
+    """
+    The class that handles the Movie discovery from IMDB and other data sources. It will automatically create
+    Movie objects, Actor objects, and Director objects, and store them in the database via their corresponding
+    repositories.
+    """
+    def __init__(self):
+        pass
