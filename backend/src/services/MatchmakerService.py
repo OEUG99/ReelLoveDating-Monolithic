bbad1698738ebfd2dd23d3835f0ecacc67f0f51f
O. Eugenio <ome18@fsu.edu>
2023-04-20 04:02:10 -0400

    Added boilerplate code for the Movie Discovery service and a Matchmaker service.

---
+++ backend/src/services/MatchmakerService.py
@@ -0,0 +1,10 @@
+
+class MatchmakerService:
+    """
+    This class is responsible for matching profiles to each other. It will run a matching algorithm on the profiles
+    and will add any matches to the Match table in the database.
+    """
+
+    def __init__(self):
+        pass
+
